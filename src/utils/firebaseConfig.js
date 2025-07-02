97d75832aead923b0be54efa7d69c98199d6e3aa
Arpit <Arpit123@.com>
2025-07-03 00:50:57 +0530

    .env done

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -9,14 +9,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCzwvRB31hYOxqDHUtij-vPhqJQucDhD5U",
-  authDomain: "cryptilo.firebaseapp.com",
-  databaseURL: "https://cryptilo-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cryptilo",
-  storageBucket: "cryptilo.firebasestorage.app",
-  messagingSenderId: "321400328819",
-  appId: "1:321400328819:web:cadf22c1bd25c498779006",
-  measurementId: "G-TJB6W79KPE"
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
